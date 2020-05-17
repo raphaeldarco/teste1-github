833af841919027b5f781342cb404497b1f20501b
Raphael Darco <raphaeldarco@gmail.com>
2020-05-17 20:06:02 -0300

    Acrescentata mensagem de bom dia e OK

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,9 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Bom dia !");
+		System.out.println("OK!");
+		
 	}
 
 }
